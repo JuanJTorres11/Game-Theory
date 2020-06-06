146b43410e2bb7f29ea73b39980c41516399a26c
Juan José Torres <juanjtorres@pm.me>
2020-06-06 18:08:14 -0500

    best strategy

--- program.py
+++ program.py
@@ -1,7 +1,5 @@
-import math, pprint
 players = 0
 tree = {}
-pp = pprint.PrettyPrinter(indent=2)
 with open("./data.txt") as dataFile:
     players = int(dataFile.readline().split(":")[1].strip())
     dataFile.readline()
@@ -14,15 +12,14 @@ with open("./data.txt") as dataFile:
             tree[arr[0]] = {'isLeaf': 'T', 'payoffs': arr[2].replace("\n","")[1:-1].split(',')}
         text = dataFile.readline()
 
-def induction(node):
-    string = ""
+def induction(node:str) -> None :
     if (tree[node]['isLeaf'] == 'F'):
-        pair = None
+        pair = () # set of (best choice at the moment, payoff of that choice)
         for i in tree[node]['children']:
             induction(i)
-            if pair != None:
-                num1 = tree[i]['best'][1][tree[node]['player']-1]
-                num2 = pair[1][tree[node]['player']-1]
+            if len(pair) != 0:
+                num1 = int(tree[i]['best'][1][tree[node]['player']-1])
+                num2 = int(pair[1][tree[node]['player']-1])
                 if ( num1 > num2 ):
                     pair = (i, tree[i]['best'][1])
             else:
@@ -30,6 +27,17 @@ def induction(node):
         tree[node]['best'] = pair
     else:
         tree[node]['best'] = (node, tree[node]['payoffs'])
+
+def bestStrategy () -> str :
+    msj = f"{'Node':^10}{'Player':^10}{'Best Option':^10}\n"
+    node = tree['0']
+    current = 0
+    while node['isLeaf'] == 'F':
+        msj += f"{current:^10}{node['player']:^10}{node['best'][0]:^10}\n"
+        current = node['best'][0]
+        node = tree[node['best'][0]]
+    msj += f"The payoff is {node['best'][1]} in the node {current}"
+    return msj
 
 print('Wellcome to the Game theory application!!')
 #print('Please input a node id:')
@@ -41,4 +49,4 @@ print('Wellcome to the Game theory application!!')
 
 #print("\nSelected node is ({})\n".format(node))
 induction('0')
-pp.pprint(tree)
+print(bestStrategy())
